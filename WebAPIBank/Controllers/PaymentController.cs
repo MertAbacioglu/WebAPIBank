eeed62613a7a9a37854043ea7347cf8a909d3302
MertAbacioglu <75869889+MertAbacioglu@users.noreply.github.com>
2021-08-15 02:34:09 +0300

    Add project files.

---
+++ WebAPIBank/Controllers/PaymentController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIBank.DesignPatterns.SingletonPattern;
+using WebAPIBank.DTOClasses;
+using WebAPIBank.Models.Context;
+using WebAPIBank.Models.Entities;
+
+namespace WebAPIBank.Controllers
+{
+    public class PaymentController : ApiController
+    {
+        MyContext _db;
+        public PaymentController()
+        {
+            _db = DBTool.DBInstance;
+        }
+        #region Test
+        //public List<PaymentDTO> GetAll()
+        //{
+        //    return _db.Cards.Select(x => new PaymentDTO
+        //    {
+        //        CardUserName = x.CardUserName
+
+        //    }).ToList();
+        //}
+        #endregion
+
+        [HttpPost]
+        public IHttpActionResult ReceivePayment(PaymentDTO item)
+        {
+            CustomerCardInfo cif = _db.Cards.FirstOrDefault(x => x.CardNumber == item.CardNumber && x.SecurityNumber == item.SecurityNumber && x.CardUserName == item.CardUserName && x.CardExpiryYear == item.CardExpiryYear && x.CardExpiryMonth == item.CardExpiryMonth);
+
+            if (cif != null)
+            {
+                if (cif.CardExpiryYear < DateTime.Now.Year)
+                {
+                    return BadRequest("Expired Card");
+                }
+                else if (cif.CardExpiryYear == DateTime.Now.Year)
+                {
+                    if (cif.CardExpiryMonth < DateTime.Now.Month)
+                    {
+                        return BadRequest("Expired Card");
+                    }
+                    if (cif.Balance >= item.ShoppingPrice)
+                    {
+                        return Ok();
+                    }
+                    else
+                    {
+                        return BadRequest("Balance Exceeded");
+                    }
+
+                }
+
+                if (cif.Balance >= item.ShoppingPrice)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    return BadRequest("Balance Exceeded");
+                }
+            }
+
+            else
+            {
+                return BadRequest("Card Not Found");
+            }
+        }
+
+    }
+}
---
+++ WebAPIBank/Models/Context/MyContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebAPIBank.Models.Entities;
+using WebAPIBank.Models.Init;
+
+namespace WebAPIBank.Models.Context
+{
+    public class MyContext:DbContext
+    {
+        public MyContext() : base("myConnection")
+        {
+            Database.SetInitializer(new MyInit()); //ctor'unu tetikliyoruz new'leyerek.
+
+        }
+
+        public DbSet<CustomerCardInfo> Cards { get; set; }
+    }
+}
